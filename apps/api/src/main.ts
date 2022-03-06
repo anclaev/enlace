6675fbe84df7029b04c4af54e869680963126e80
anclaev <iahugo@yandex.ru>
2022-03-07 03:00:57 +0400

    Fix package metadata.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+const bootstrap = async () => {
   const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+  const prefix = 'api';
+
+  const app: INestApplication = (
+    await NestFactory.create(AppModule)
+  ).setGlobalPrefix(prefix);
+
+  app
+    .listen(port)
+    .finally(() =>
+      Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${prefix}`
+      )
+    );
+};
 
 bootstrap();
