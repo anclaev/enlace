c5f4253281d4945480e0f2970deee8bbd39cf515
anclaev <iahugo@yandex.ru>
2022-03-07 21:07:00 +0400

    Project cleaning.

--- apps/client/src/app/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'enlace-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass'],
-})
-export class HeaderComponent {}
