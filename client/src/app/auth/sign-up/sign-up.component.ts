0c66805cdb0e0d3108b3665f60dae79aa7c60503
anclaev <iahugo@yandex.ru>
2022-03-09 11:13:14 +0000

    Create user logic.

--- client/src/app/auth/sign-up/sign-up.component.ts
+++ client/src/app/auth/sign-up/sign-up.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core'
 
 import { AuthCredentials } from '@common'
 
+import { CreateUserGQL } from 'src/generated-types'
+
 @Component({
   selector: 'enlace-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.sass'],
 })
 export class SignUpComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly createUserGql: CreateUserGQL) {}
 
   ngOnInit(): void {}
 
   signUp({ email, password }: AuthCredentials) {
-    console.log(email, ' ', password)
+    this.createUserGql
+      .mutate({
+        createUserData: {
+          email,
+          password,
+        },
+      })
+      .subscribe((res) => console.log(res))
   }
 }
