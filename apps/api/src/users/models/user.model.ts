d52ba5996a1ad4305ae8bfce548c763346012f83
SAMARIN Artem <Artyom.Samarin@vaz.ru>
2022-03-09 11:38:51 +0400

    Cleaning up a project.

--- apps/api/src/users/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ObjectType } from '@nestjs/graphql'
-
-import { Abstract } from '@enlace/models'
-
-@ObjectType()
-export class User extends Abstract {
-  @Field()
-  readonly email: string
-}
