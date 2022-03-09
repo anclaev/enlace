2e839f983fc767353e7e9cf09bd0b8dcc6121420
anclaev <iahugo@yandex.ru>
2022-03-09 08:18:44 +0000

    Empty angular project.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
